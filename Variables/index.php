21834201d876f14c26501b129640579ccf274639
Adamaris Marin <adamarismarin92@gmail.com>
2024-12-21 15:51:47 -0600

    commit 2

---
+++ Variables/index.php
@@ -0,0 +1,15 @@
+<?php
+ $nombre "Adamaris";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> <?php echo "Hola mundo mi nombre es " . $nombre; ?> </h1>
+</body>
+</html>
